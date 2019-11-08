ddaa2fe28d58ed22fe81b0744da893e8ff2a85ba
Daniel Reina <dannyflatiron@gmail.com>
2019-11-08 01:26:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(' and ')
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(', ')
 
+
+
+  end
+  # binding.pry
 end
